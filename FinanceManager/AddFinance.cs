14ce8106f268660cc4d6e4e1c071054042cf7368
menakeobhasha <menakeobhasha1@gmail.com>
2022-07-10 22:57:32 +0530

    Add AddFinance Form

--- FinanceManager/AddFinance.cs
+++ FinanceManager/AddFinance.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace FinanceManager
 {
-    public partial class Form1 : Form
+    public partial class AddFinance : Form
     {
-        public Form1()
+        public AddFinance()
         {
             InitializeComponent();
         }
